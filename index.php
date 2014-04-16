81384da405af085e82a1d61c55c8efb2ee7db77b
evanildobarros <evanildobarros@gmail.com>
2014-04-15 23:55:10 -0300

    versao 1.3

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <h3 class="panel-title">Acesso</h3>
   </div>
   <div class="panel-body">
-    <form class="form-horizontal" name="form1" method="POST" action="./Home/index.php">
+    <form class="form-horizontal" name="form1" method="POST" action="./Programacao/log.php">
   <fieldset>
     <legend>Acesso</legend>
    <div class="form-group"><label  class="col-md-2 control-label">Usuário</label>
