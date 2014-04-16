bc586431ee72d866c14a6a7043d671c3c9cd73d9
evanildobarros <evanildobarros@gmail.com>
2014-04-16 00:02:08 -0300

    versao 1.3

--- Home/painel.php
+++ Home/painel.php
@@ -44,46 +44,241 @@ if (!isset($_SESSION)) {
 
 <body>
 
-<div class="navbar navbar-default">
-  <div class="navbar-header">
-    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-inverse-collapse">
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </button>
-    <a class="navbar-brand" href="../Home/painel.php">Home</a>
-  </div>
-  <div class="navbar-collapse collapse navbar-inverse-collapse">
-    <ul class="nav navbar-nav">
-       <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Cadastro <b class="caret"></b></a>
-        <ul class="dropdown-menu">
-          <li><a href="../Cadastros/cadastro_alunos.php">Aluno</a></li>
-          <li><a href="../Cadastros/cadastro_instrutor.php">Instrutor</a></li>
-         </ul>
+<div id='nav'>
+    <div id='navleft'>
+      <ul>
+        <li><a href="index.php">Home</a></li>
+      <li><a href='#'>Cadastro</a>
+      <ul>
+      <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3 )
+      { echo "<a href='../Cadastros/cadastro_alunos2.php'>Alunos </a>"; } ?></li>
+      
+      
+      <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3)
+      { echo "<a href='../Cadastros/cadastro_instrutor2.php'>Instrutor </a>"; } ?></li>
+      
+      
+      <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3)
+      { echo "<a href='../Cadastros/cadastro_fornecedor2.php'>Fornecedor </a>"; } ?></li>
+      
+      
+      <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3)
+      { echo "<a href='../Cadastros/cadastro_pagamento2.php'>Forma de Pagamento </a>"; } ?></li>
+      
+      
+      <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3)
+      { echo "<a href='../Cadastros/cadastro_veiculo2.php'>Ve&iacute;culos  </a>"; } ?></li>
+      
+      </ul>
       </li>
-           <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Financeiro <b class="caret"></b></a>
-        <ul class="dropdown-menu">
-          <li><a href="../Consultas/consulta_adicionar_servico_aluno.php">Adicionar Serviços</a></li>
-          <li><a href="../Financeiro/balaco_financeiro.php">Balanço Geral</a></li>
-          <li><a href="../Financeiro/contas_paga.php">Contas pagas</a></li>
-          <li><a href="../Financeiro/contas_receber.php">Contas à Receber</a></li>
-          <li><a href="../Financeiro/despesas.php">Despesas</a></li>
-          <li><a href="../Financeiro/devedor.php">Devedor</a></li>
-         </ul>
+      
+      <li><a href="">Curso te&oacute;rico</a>
+      <ul>
+    
+            
+            <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3)
+      { echo "<a href='../Consultas/aula_teorica2.php'>Aulas te&oacute;ricas</a>"; } ?></li>
+            
+             <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3)
+      { echo "<a href='../Consultas/adicionar_carga_teorica.php'>Carga horÃ¡ria</a>"; } ?></li>
+            
+            <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3)
+      { echo "<a href='../Consultas/adicionar_legislacao.php'>Marca exame (DETRAN)</a>"; } ?></li>
+                        
+             <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3)
+      { echo "<a href='../Consultas/prova_legislacao2.php'>Data exames de legisla&ccedil;&atilde;o</a>"; } ?></li>
+            
+              <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3)
+      { echo "<a href='../Consultas/consulta_redimento_legislacao.php'>Rendimento do aluno</a>"; } ?></li>
+            
+            
+            <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3)
+      { echo "<a href='../Consultas/lista_preset.php'>Lista de presen&ccedil;a</a>"; } ?></li>
+            
+            
+              <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3)
+      { echo "<a href='../Consultas/consulta_aprovados_legislacao.php'>Alunos Aprovados</a>"; } ?></li>  
+            
+              <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3)
+      { echo "<a href='../Consultas/consulta_reprovados_legislacao.php'>Alunos Reprovados</a>"; } ?></li>            
+            
+            </ul>
+      
+      
       </li>
-           <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Auto Escola <b class="caret"></b></a>
-        <ul class="dropdown-menu">
-          <li><a href="../Consultas/consulta_adicionar_agendamento_aluno.php">Agendamento</a></li>
-          </ul>
+            
+             <li><a href="">Simulador</a>
+      <ul>
+            
+            <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3 ){
+        echo "<a href='../Agendamento/calendar2.php'> Agendamento
+      </a>"; } ?></li>
+            
+            <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3 ){
+        echo "<a href='../Consultas/adicionar_carga_simulador.php'> Carga hor&aacute;ria simulador
+      </a>"; } ?></li>
+            
+             <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3 ){
+        echo "<a href='../Consultas/alunos_agendados_simulador2.php'> Rela&ccedil;&atilde;o de alunos agendados
+      </a>"; } ?></li>
+           
+      </ul>
       </li>
-
-
-    </ul>
+            
+            <li><a href="#">Aulas prÃ¡ticas</a>
+      <ul>
+    
+            
+            <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3)
+      { echo "<a href='../Agendamento/calendar.php'>Agendamento</a>"; } ?></li>
+            
+       <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3)
+      { echo "<a href='../Consultas/relacao_alunos.php'>Instrutor / rela&ccedil;&atilde;o de alunos</a>"; } ?></li>
+            
+               <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3 ){
+        echo "<a href='../Consultas/alunos_agendados2.php'> Rela&ccedil;&atilde;o de alunos agendados
+      </a>"; } ?></li>
+            
+            
+             <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3)
+      { echo "<a href='../Consultas/adicionar_carga_pratica.php'>Carga hor&aacute;ria </a>"; } ?></li>
+            
+             <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3)
+      { echo "<a href='../Consultas/adicionar_trafego.php'>Marca exame (DETRAN)</a>"; } ?></li>
+            
+             <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3)
+      { echo "<a href='../Consultas/prova_trafego2.php'>Data exames de tr&aacute;fego</a>"; } ?></li>
+            
+            
+            <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3)
+      { echo "<a href='../Consultas/consulta_aluno_aprovado.php'>Rendimento do aluno</a>"; } ?></li>
+            
+             <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3)
+      { echo "<a href='../Consultas/consulta_aprovados_trafego.php'>Alunos Aprovados </a>"; } ?></li>
+             <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3)
+      { echo "<a href='../Consultas/consulta_reprovados_trafego.php'>Alunos Reprovados</a>"; } ?></li>
+            </ul>
+      </li>
+            
+      <li><a href="#">Financeiro</a>
+      <ul>
+            <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 or $_SESSION['chave']==3 ){
+        echo "<a href='../Consultas/adicionar_servicos.php'>Registrar Servi&ccedil;os
+      </a>"; } ?></li>
+      <li><?php if ($_SESSION['chave']==1){ echo "<a href='../Financeiro/balaco_financeiro.php'>Balan&ccedil;o Geral
+      </a>"; } ?></li>
+      <li><?php if ($_SESSION['chave']==1){ echo "<a href='../Financeiro/contas_paga.php'>Contas Pagas
+      </a>"; } ?></li>
+      <li><?php if ($_SESSION['chave']==1){ echo "<a href='../Financeiro/contas_receber2.php'>Contas a Receber
+      </a>"; } ?></li>
+      <li><?php if ($_SESSION['chave']==1){ echo "<a href='../Financeiro/despesas2.php'>Lista de despesas
+      </a>"; } ?></li>
+      <li><?php if ($_SESSION['chave']==1){ echo "<a href='../Financeiro/devedor2.php'>Clientes em D&eacute;bito
+      </a>"; } ?></li>
+            
+            <li><?php if ($_SESSION['chave']==1){ echo "<a href='../Consultas/consulta_fluxo_caixa.php'>Fluxo de Caixa
+      </a>"; } ?></li>
+            
+      <li><?php if ($_SESSION['chave']==1){ echo "<a href='../Cadastros/cadastro_registro_despesas.php'>Registrar Despesas
+      </a>"; } ?></li>
+      </ul>
+      </li>
+            <li><a href="#">Estat&iacute;sticas</a>
+      <ul>
+            
+            <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2  ){
+        echo "<a href='../Estatisticas_finaceira/total_lotacao_alunos_mes.php'> Alunos matriculados por m&ecirc;s
+      </a>"; } ?></li>
+            
+            <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 ){
+        echo "<a href='../Estatisticas_finaceira/total_lotacao_alunos_lotacao.php'> Total de recita por lota&ccedil;&atilde;o
+      </a>"; } ?></li>
+            <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2  ){
+        echo "<a href='../Estatisticas_finaceira/total_lotacao_despesas.php'> Total de Despesas por lota&ccedil;&atilde;o
+      </a>"; } ?></li>
+            
+            <li><?php if ($_SESSION['chave']==1 or $_SESSION['chave']==2 ){
+        echo "<a href='../Estatisticas_finaceira/total_lotacao_despesas_mes.php'> Total de despesas por m&ecirc;s
+      </a>"; } ?></li>
+      
+      </ul>
+      </li>
+            
+            <li><a href="#">Administra&ccedil;&atilde;o</a>
+      <ul>
+            
+           
+             <li> <?php if ($_SESSION['chave']==1){ 
+      echo "<a href='../Cadastros/cadastro_usuario2.php'>Cadastrar Usu&aacute;rio
+      </a>"; } ?>
+            </li>
+            
+            </li>
+             <li> <?php if ($_SESSION['chave']==1){ 
+      echo "<a href='../Consultas/edita_excliur_alunos.php'>Editar | Excluir Alunos
+      </a>"; } ?>
+            </li>
+      
+             <li> <?php if ($_SESSION['chave']==1){ 
+      echo "<a  href='../Consultas/admin.php'>A&ccedil;&otilde;es do Usu&aacute;rio
+      </a>"; } ?>
+            </li>
+            
+             <li> <?php if ($_SESSION['chave']==1){ 
+      echo "<a  href='../Home/log.php'>Excluir opera&ccedil;&otilde;es
+      </a>"; } ?>
+            </li>
+            
+      </ul>
+      </li>
+            <li><a href="../aviso.php?id=2">Sair</a></li>
+      </ul>
   </div>
 </div>
+    </td>
+  </tr>
+<tr>
+<td bgcolor="#FFFFFF">
+<div class="grid">
+                          <?php require('../consultas/consulta_alunos.php'); 
+            @mysql_free_result($Recordset1);
+            ?>
+                          <?php
+            @mysql_free_result($total2);
+            ?>
+            </div>
+            <div class="titulo_msn">Painel de Controle</div>
+            <div class="box_msn">
+              <fieldset>
+                <legend>Mensagem do Dia</legend>
+                <?php echo $_SESSION['mensagemdodia']; ?>
+              </fieldset><br />
+<br />
+
+              Usuï¿½rio Logado no Momento &raquo;
+            <?php include ("online.php"); ?>
+              <br>
+              <hr />
+            <br>
+              Vocï¿½ faz parte do Grupo: <font color="#CC3333"><strong><br>
+                <br>
+                <?php echo $grupo; ?></strong></font>
+              <hr />
+              Total de Alunos Matriculados:<br />
+              <?php echo $row_Recordset1['total']; ?>
+              
+              <hr />
+              Total de Alunos Matriculados por dia:<br />
+             
+              <?php echo $row_total2['total2']; ?>
+            </div></td>
+            </tr>
+            <tr>
+            <td bgcolor="#333" align="center"><font color="#FFFFFF">Mhs Solucï¿½es Web Contato: (98) 8800-3198 | 3288046 <br>
+            email:mhssloucoesweb@hotmail.com &copy; 2014 copyright</font></td>
+            </tr>
+    </table>
+
 		
    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
